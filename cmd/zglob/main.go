d27b95ac602e6723fd8b2583d529212a80037081
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-12-30 19:34:08 +0900

    fix zglob command

--- cmd/zglob/main.go
+++ cmd/zglob/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,15 +9,21 @@ import (
 )
 
 func main() {
+	var d bool
+	flag.BoolVar(&d, "d", false, "with directory")
+	flag.Parse()
 	for _, arg := range os.Args[1:] {
 		matches, err := zglob.Glob(arg)
 		if err != nil {
 			continue
 		}
 		for _, m := range matches {
-			if fi, err := os.Stat(m); err == nil && fi.Mode().IsRegular() {
-				fmt.Println(m)
+			if !d {
+				if fi, err := os.Stat(m); err == nil && fi.Mode().IsDir() {
+					continue
+				}
 			}
+			fmt.Println(m)
 		}
 	}
 }
